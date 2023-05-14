4663108f5393b0a8a6ee49da98aa242497ba3b98
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-05-14 17:45:06 +0200

    + database module

---
+++ src/database/database.providers.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
