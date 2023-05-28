6c34c3bcbc79dae099a8bf23eb9b1a5958f49cfe
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-05-28 09:07:56 +0200

    fix DB env

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,12 +7,12 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         type: 'mysql',
         host: process.env.DB_HOST,
-        port: 3306,
+        port: +process.env.DB_PORT,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: process.env.ENVIROMENT === 'dev',
       });
 
       return dataSource.initialize();
