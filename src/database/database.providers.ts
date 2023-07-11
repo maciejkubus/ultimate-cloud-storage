02cb1d346b23bbc78ba7af6e8bfe92946f0e264d
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-07-11 16:47:43 +0200

    DEV CHANGE sqlite -> mysql

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,11 +4,21 @@ export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
+      // const devOptions: DataSourceOptions = {
+      //   type: 'better-sqlite3',
+      //   database : process.env.SQLITE_DB,
+      //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      //   synchronize: true,
+      // };
       const devOptions: DataSourceOptions = {
-        type: 'better-sqlite3',
-        database : process.env.SQLITE_DB,
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: process.env.ENVIROMENT === 'dev',
+        synchronize: true,
       };
       const prodOptions: DataSourceOptions = {
         type: 'mysql',
@@ -18,7 +28,7 @@ export const databaseProviders = [
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: process.env.ENVIROMENT === 'dev',
+        synchronize: true,
       };
       const dataSource = new DataSource(
         process.env.ENVIROMENT === 'prod' ? prodOptions : devOptions,
