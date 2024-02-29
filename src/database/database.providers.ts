b2e0d4b82ccfb8aa3619f3f4074a2ccbb59429bd
Maciej Kubus <maciek.krol.zycia@gmail.com>
2024-02-29 22:22:34 +0100

    + dev db sqlite

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,22 +4,22 @@ export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
-      // const devOptions: DataSourceOptions = {
-      //   type: 'better-sqlite3',
-      //   database : process.env.SQLITE_DB,
-      //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      //   synchronize: true,
-      // };
       const devOptions: DataSourceOptions = {
-        type: 'mysql',
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        type: 'better-sqlite3',
+        database : process.env.SQLITE_DB,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       };
+      // const devOptions: DataSourceOptions = {
+      //   type: 'mysql',
+      //   host: process.env.DB_HOST,
+      //   port: +process.env.DB_PORT,
+      //   username: process.env.DB_USER,
+      //   password: process.env.DB_PASSWORD,
+      //   database: process.env.DB_NAME,
+      //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      //   synchronize: true,
+      // };
       const prodOptions: DataSourceOptions = {
         type: 'postgres',
         host: process.env.DB_HOST,
