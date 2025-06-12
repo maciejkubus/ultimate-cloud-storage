9c6353bf20f96753b9237bfe79cdf9c4a3a9b9aa
Maciej Kubus <maciek.krol.zycia@gmail.com>
2025-06-12 10:25:09 +0200

    fix

--- src/emotions/dto/create-emotion.dto.ts
+++ src/emotions/dto/create-emotion.dto.ts
@@ -1,10 +1,9 @@
-import { IsString, Length } from "class-validator";
+import { IsString } from "class-validator";
 
 export class CreateEmotionDto {
   @IsString()
   name: string;
 
   @IsString()
-  @Length(1, 1)
   emoticon: string;
 }
